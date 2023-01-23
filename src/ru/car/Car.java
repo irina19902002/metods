aa1104ef379343378c3d35e8eade85bfa1bc0a22
Irina Smirnova <smirnovaiv20.02@gmail.com>
2023-01-23 23:23:43 +0300

    5 методов

--- src/ru/car/Car.java
+++ src/ru/car/Car.java
@@ -11,10 +11,33 @@ package ru.car;
 
 
 public class Car {
-    String color;
+    private String color;
     String type;
     int year;
+
+
+    public void start(){
+        System.out.println("Автомобиль заведен");
+    }
+
+    public void off(){
+        System.out.println("Автомобиль заглушен");
+    }
+
+    public int returYear(int year){
+        this.year = year;
+        return year;
+    }
+    public String returType(String type){
+        this.type = type;
+        return type;
+    }
+
+    public String getColor(){
+        return color;
+    }
     public Car(){
         color="green";
+
     }
 }
--- src/ru/car/Main.java
+++ src/ru/car/Main.java
@@ -1,5 +1,14 @@
 package ru.car;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        car1.start();
+        car2.off();
+        System.out.println(car1.getColor());
+        System.out.println(car2.getColor());
+        System.out.println("Автомобиль car1 "+" " +car1.returYear(2020)+car1.returType(" Новый"));
+        System.out.println("Автомобиль car2 "+" " +car2.returYear(1990)+car2.returType(" Б.У"));
+    }
 }
